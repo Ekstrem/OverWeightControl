616d572099855fc2ed84cbe968d76411cf3012fd
Ekstrem <about47@ya.ru>
2018-04-19 23:30:36 +0300

    Core.RemoteInteraction

---
+++ source/Core/RemoteInteraction/IRemoteInteraction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using OverWeightControl.Core.FileTransfer;
+
+namespace OverWeightControl.Core.RemoteInteraction
+{
+    /// <summary>
+    /// Интерфейс передачи файлов.
+    /// </summary>
+    interface IFileTransfer
+    {
+        /// <summary>
+        /// Инициирует соединение трасфера файлов.
+        /// Пересылает список файлов.
+        /// </summary>
+        /// <param name="sendList">Список информации о передаваемых файлах.</param>
+        /// <returns>Список информации о не дошедших файлах.</returns>
+        ICollection<FileTransferInfo> SendList(ICollection<FileTransferInfo> sendList);
+
+        /// <summary>
+        /// Отправка файла.
+        /// </summary>
+        /// <param name="stream">Данные файла.</param>
+        /// <returns>Колличество полученных файлов.</returns>
+        long SendFile(byte[] stream);
+    }
+}
