787946a91a3b7bf1b9b0b300b0827eb19a7ef901
Ekstrem <about47@ya.ru>
2018-04-19 23:30:11 +0300

    Core.Console

---
+++ source/Core/Console/DefaultConsoleService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OverWeightControl.Core.Console
+{
+    public class DefaultConsoleService : IConsoleService
+    {
+        /// <summary>
+        /// bool значение - новое и непрочитано.
+        /// </summary>
+        protected IDictionary<ConsoleMessage, bool> _log;
+
+        public DefaultConsoleService()
+        {
+            _log = new ConcurrentDictionary<ConsoleMessage, bool>();
+        }
+
+        public void AddEvent(string message)
+        {
+            AddEvent(new ConsoleMessage(message));
+        }
+
+        public void AddEvent(string message, ConsoleMessageType type)
+        {
+            AddEvent(new ConsoleMessage(message, type));
+        }
+
+        public void AddEvent(string message, ConsoleMessageType type, DateTime time)
+        {
+            var cm = new ConsoleMessage(message, type) { Time = time };
+            AddEvent(cm);
+        }
+
+        public void AddEvent(ConsoleMessage message)
+        {
+            _log.Add(message, true);
+        }
+
+        public void AddException(Exception e)
+        {
+            AddEvent(
+                message: $"{e.Message}\n{e?.InnerException?.Message ?? string.Empty}",
+                type: ConsoleMessageType.Exception);
+
+        }
+
+        public virtual IDictionary<ConsoleMessage, bool> Flush()
+        {
+            foreach (var message in _log.Keys.Where(f=> _log[f]))
+            {
+                string clipMessage =
+                    $"At {message.Time.ToShortTimeString()} event {((int)message.Type)}, message: {message.Message}";
+                _log[message] = false;
+                System.Console.WriteLine(clipMessage);
+            }
+
+            
+            System.Console.ReadKey();
+            return _log;
+        }
+    }
+}
---
+++ source/Core/Console/IConsoleService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OverWeightControl.Core.Console
+{
+    public interface IConsoleService
+    {
+        void AddEvent(string message);
+        void AddEvent(string message, ConsoleMessageType type);
+        void AddEvent(string message, ConsoleMessageType type, DateTime time);
+        void AddEvent(ConsoleMessage message);
+        void AddException(Exception e);
+        IDictionary<ConsoleMessage, bool> Flush();
+    }
+
+    public enum ConsoleMessageType
+    {
+        Information = 0,
+        Trace = 1,
+        Exception = 2
+    }
+
+    public class ConsoleMessage
+    {
+        public ConsoleMessage(
+            string message,
+            ConsoleMessageType type = ConsoleMessageType.Information)
+        {
+            Message = message;
+            Type = type;
+            Time = DateTime.Now;
+        }
+
+        public string Message { get; set; }
+        public ConsoleMessageType Type { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
