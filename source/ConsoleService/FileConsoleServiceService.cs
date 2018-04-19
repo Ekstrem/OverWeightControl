dc0b56b168ad26b8b88104b9e6731d5f62d0e060
Ekstrem <about47@ya.ru>
2018-04-19 23:37:34 +0300

    ConsoleService

---
+++ source/ConsoleService/FileConsoleServiceService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using OverWeightControl.Core.Console;
+
+namespace OverWeightControl.Core.ConsoleService
+{
+    public class FileConsoleServiceService : DefaultConsoleService, IConsoleService
+    {
+        public override IDictionary<ConsoleMessage, bool> Flush()
+        {
+            var jLog = JsonConvert.SerializeObject(_log);
+            System.Console.Write(jLog);
+            return _log;
+        }
+    }
+}
