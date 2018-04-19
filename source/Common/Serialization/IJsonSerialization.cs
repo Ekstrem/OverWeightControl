7abc8d0cc1f5873c95d836eb2ffa5c616482d202
Ekstrem <about47@ya.ru>
2018-04-19 22:30:04 +0300

    Common: setialization.
    Встроеная в домен json-сериализация.

---
+++ source/Common/Serialization/IJsonSerialization.cs
@@ -0,0 +1,31 @@
+﻿namespace OverWeightControl.Common.Serialization
+{
+    /// <summary>
+    /// Интерфейс сериализации.
+    /// </summary>
+    /// <typeparam name="TEntity">Объект сериализации.</typeparam>
+    public interface IJsonSerialization<TEntity>
+    {
+        /// <summary>
+        /// Загрузка файла из json-строки
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns>Десереализованный объект.</returns>
+        TEntity LoadFromJson(string json);
+
+        /// <summary>
+        /// Загрузка тэга из json.
+        /// </summary>
+        /// <param name="json">json представление объекта.</param>
+        /// <param name="tag">Тэг для чтения.</param>
+        /// <returns>Свойство по тэгу.</returns>
+        object LinqToJson(string json, string tag);
+
+        /// <summary>
+        /// Сохранение в json-формат.
+        /// </summary>
+        /// <param name="entity">Сохраняемый объект.</param>
+        /// <returns>json строка.</returns>
+        string ToJson();
+    }
+}
---
+++ source/Common/Serialization/ParentBehavior.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace OverWeightControl.Common.Serialization
+{
+    /// <summary>
+    /// Базовый класс.
+    /// </summary>
+    /// <typeparam name="TEntity">Дочерний класс.</typeparam>
+    public class ParentBehavior<TEntity> : IJsonSerialization<TEntity>
+    {
+        protected ParentBehavior()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        /// <summary>
+        /// Unique identifier.
+        /// </summary>
+        [JsonProperty]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Загрузка файла из json-строки
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns>Десереализованный объект.</returns>
+        public TEntity LoadFromJson(string json)
+        {
+            var jss = new JsonSerializerSettings { Formatting = Formatting.Indented };
+            var obj = JsonConvert
+                .DeserializeObject<TEntity>(json, jss);
+            return obj;
+        }
+
+        /// <summary>
+        /// Загрузка тэга из json.
+        /// </summary>
+        /// <param name="json">json представление объекта.</param>
+        /// <param name="tag">Тэг для чтения.</param>
+        /// <returns>Свойство по тэгу.</returns>
+        public object LinqToJson(string json, string tag)
+        {
+            return JObject.Parse(json)[tag];
+        }
+
+        /// <summary>
+        /// Сохранение в json-формат.
+        /// </summary>
+        /// <returns>json строка.</returns>
+        public string ToJson()
+        {
+            var jss = new JsonSerializerSettings {Formatting = Formatting.Indented};
+            return JsonConvert.SerializeObject(this, jss);
+        }
+    }
+}
