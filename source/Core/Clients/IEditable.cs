4c15712a6306664e9742b3e7918ac0e32dfa8c34
Ekstrem <about47@ya.ru>
2018-04-19 23:31:10 +0300

    Core.Clients

---
+++ source/Core/Clients/IEditable.cs
@@ -0,0 +1,21 @@
+﻿namespace OverWeightControl.Core.Clients
+{
+    /// <summary>
+    /// Интерфейс манипуляции на формах и контролах.
+    /// </summary>
+    /// <typeparam name="TEditableEntity">Редактируемая сущность.</typeparam>
+    public interface IEditable<TEditableEntity>
+    {
+        /// <summary>
+        /// Загрузка данных в контрол.
+        /// </summary>
+        /// <param name="data">Обновляемые данные.</param>
+        bool LoadData(TEditableEntity data);
+
+        /// <summary>
+        /// Получение данных из контрола после редактирования.
+        /// </summary>
+        /// <returns>Обновляемые данные.</returns>
+        TEditableEntity UpdateData();
+    }
+}
