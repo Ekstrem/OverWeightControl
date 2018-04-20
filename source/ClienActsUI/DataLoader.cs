6df03bb03206e2c76f822a864e959cf6be09e4a4
Ekstrem <about47@yandex.ru>
2018-04-20 11:48:23 +0300

    ActsUI: DataLoader updates.

--- source/ClienActsUI/DataLoader.cs
+++ source/ClienActsUI/DataLoader.cs
@@ -6,23 +6,23 @@ namespace OverWeightControl.Clients.ActsUI
 {
     internal static class DataLoader
     {
-        internal static void Load(
+        internal static void LoadData(
             this TextBox control, RecognizedValue value) => 
             control.Text = value.Value;
 
-        internal static void Load(
+        internal static void LoadData(
             this TextBox control, string value) => control.Text = value;
-        internal static void Load(
+        internal static void LoadData(
             this TextBox control, int value) => control.Text = value.ToString();
-        internal static void Load(
+        internal static void LoadData(
             this TextBox control, double value) =>
             control.Text = value.ToString(CultureInfo.CurrentCulture);
-        internal static void Load(
+        internal static void LoadData(
             this TextBox control, float value) =>
             control.Text = value.ToString(CultureInfo.CurrentCulture);
 
 
-        internal static RecognizedValue Update(this TextBox control)
+        internal static RecognizedValue UpdateData(this TextBox control)
         {
             return new RecognizedValue(control.Text);
         }
