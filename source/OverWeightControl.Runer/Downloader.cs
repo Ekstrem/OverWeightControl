9caab5e1b60849c0edd5e41a42906baaedec00e4
Ekstrem <about47@yandex.ru>
2018-05-23 19:15:46 +0300

    OWC.Runner: add logic to client.

--- source/OverWeightControl.Runer/Downloader.cs
+++ source/OverWeightControl.Runer/Downloader.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OverWeightControl.Core.Console;
 using OverWeightControl.Core.Settings;
 
--- source/OverWeightControl.Runer/UpdateClient.cs
+++ source/OverWeightControl.Runer/UpdateClient.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OverWeightControl.Core.Console;
 using OverWeightControl.Core.RemoteInteraction;
+using OverWeightControl.Core.Settings;
 using Unity.Attributes;
 
 namespace OverWeightControl.Runer
@@ -12,19 +10,31 @@ namespace OverWeightControl.Runer
     public class UpdateClient
     {
         private readonly IConsoleService _console;
+        private readonly ISettingsStorage _settings;
         private readonly Proxy _proxy;
         private readonly IDownloader _downloader;
 
         [InjectionConstructor]
         public UpdateClient(
             IConsoleService console,
+            ISettingsStorage settings,
             Proxy proxy)
         {
             _console = console;
+            _settings = settings;
             _proxy = proxy;
             _downloader = proxy.CreateRemoteProxy<IDownloader>();
         }
 
-        
+        void GetVersion()
+        {
+            int version = _downloader.GetLastVersion();
+            var files = _downloader
+                .GetFileList(version)
+                .Select(Path.GetFileName)
+                .ToDictionary(k => k, v => _downloader.DownLoadFile(version, v));
+            foreach (var file in files)
+                File.WriteAllBytes(file.Key, file.Value);
+        }
     }
 }
