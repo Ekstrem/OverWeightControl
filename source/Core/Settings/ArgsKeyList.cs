b626d126253caf3c8403b7786af30ebd1cc84b4a
Ekstrem <about47@yandex.ru>
2018-06-21 11:09:27 +0300

    Core.Settings: Add Description for configuration keis.
    part 2. Keis dictionary.

--- source/Core/Settings/ArgsKeyList.cs
+++ source/Core/Settings/ArgsKeyList.cs
@@ -1,4 +1,11 @@
-﻿namespace OverWeightControl.Core.Settings
+﻿using System;
+using System.Collections.Generic;
+using OverWeightControl.Core.FileTransfer;
+using OverWeightControl.Core.FileTransfer.Client;
+using OverWeightControl.Core.FileTransfer.RecognitionServer;
+using OverWeightControl.Core.FileTransfer.Server;
+
+namespace OverWeightControl.Core.Settings
 {
     public static class ArgsKeyList
     {
@@ -19,5 +26,38 @@
         public static string HandValidation = nameof(HandValidation);
         public static string QueuesSize = nameof(QueuesSize);
         public static string PpvkName = nameof(PpvkName);
+    }
+
+    public static class ConfigurationKeyDescription
+    {
+        private static IDictionary<string, string> _dict;
+        private static IDictionary<string, string> GetDictinary()
+        {
+            return _dict ?? (_dict = new Dictionary<string, string>
+            {
+                {ArgsKeyList.ScanPath, "Папка поиска файлов."},
+                {ArgsKeyList.StorePath, "Папка временного размещения файлов."},
+                {ArgsKeyList.BackUpPath, "Папка резерного копирования."},
+                {ArgsKeyList.AfcPath, "Папка из которой AFC ведёт распознавание."},
+                {ArgsKeyList.ArgsFileLocation, "Путь к файлу настройки."},
+                {ArgsKeyList.IsDebugMode, "Режим отладки."},
+                {ArgsKeyList.WFProcWaitingFor, "Частота обновления процессов, в сек."},
+                {ArgsKeyList.ServerName, "Адрес сервера."},
+                {ArgsKeyList.Port, "Порт сервера."},
+                {ArgsKeyList.ScanExt, "Искомые расширения файлов"},
+                {ArgsKeyList.ConnectionString, "Строка подключения к БД"},
+                {ArgsKeyList.Mode, "Модель десериализации."},
+                {ArgsKeyList.Binding, "Биндинг привязки."},
+                {ArgsKeyList.Version, "Текущая версия пакета обновлений."},
+                {ArgsKeyList.HandValidation, "Ручная потоковая верификация."},
+                {ArgsKeyList.QueuesSize, "Максимальный размер очереди в обработчике."},
+                {ArgsKeyList.PpvkName, "Имя текущего ПВК."}
+            });
+        }
+
+        public static string GetDescription(string key)
+        {
+            return GetDictinary().ContainsKey(key) ? GetDictinary()[key] : String.Empty;
+        }
     }
 }
